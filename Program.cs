d390f4a1ff66102c6f9644c2350035083fc8b0b8
Lukas Prochazka <prochazka.lu@gmail.com>
2025-08-08 15:49:38 +0200

    Add build files for C# application

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int age = 38;
+      string name = "Lukas";
+      bool isActive = true;
+
+      int x = 10, y = 20, z = 30;
+
+      Console.WriteLine($"x: {x}, y: {y}, z: {z}");
+      Console.WriteLine($"Sum of x, y, z: {x + y + z}");
+      Console.WriteLine($"Product of x, y, z: {x * y * z}");
+      Console.WriteLine($"Average of x, y, z: {(x + y + z) / 3.0}");
+      Console.WriteLine("Hello World!");
+      Console.WriteLine($"My name is {name} and I am {age} years old.");
+      if (isActive)
+      {
+        Console.WriteLine("I am currently active.");
+      }
+      else
+      {
+        Console.WriteLine("I am not active at the moment.");
+      }
+      // assign new age
+      age = 39;
+      Console.WriteLine($"My updated age is {age}.");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ RandomColorGenerator.cs
@@ -0,0 +1,11 @@
+// This Random Color Generator is designed to generate a random color, and color a console output with it.
+// As an update, the user can also specify a color and luminosity level to generate a color based on those parameters.
+using System;
+
+public class RandomColorGenerator
+{
+  public static void RandomColor()
+  {
+
+  }
+}
